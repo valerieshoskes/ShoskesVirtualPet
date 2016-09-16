dd65f01fb7f87a71fd3bd3987f162dfa09351c16
valerieshoskes <indi.vs@gmail.com>
2016-09-16 17:31:03 -0400

    complete the bulk of the project

--- Program.cs
+++ Program.cs
@@ -10,6 +10,63 @@ namespace ShoskesVirtualPet
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Virtual Pet Foster Simulator!");
+            Console.WriteLine("Type \"Quit\" at any time to quit the game");
+            Console.WriteLine("Raise all attributes to 20, and a family will adopt your pet!");
+            Console.WriteLine("If a single attribute falls to 0, animal control will assign a more suitable foster...");
+            string petType = "";
+            do {
+                Console.WriteLine("Would you like to foster a cat or a dog?");
+                petType = Console.ReadLine().ToLower();
+                QuitCheck(petType);
+            }
+            while (petType != "cat" && petType != "dog");
+            Console.WriteLine("What is your {0}'s name?", petType);
+            string petName = Console.ReadLine();
+            QuitCheck(petName);
+            VirtualPet myPet = new VirtualPet(petType, petName);
+            Console.WriteLine("{0} has been through some rough times..", petName);
+            Console.WriteLine("All their attributes are only at 5 :(");
+            Console.WriteLine("But you can help {0}! Here they are:", petName);
+            do
+            {
+                myPet.Tick();
+                string input = Console.ReadLine();
+                QuitCheck(input);
+                switch (input)
+                {
+                    case "1":
+                        myPet.Feed();
+                        break;
+                    case "2":
+                        myPet.ProvideWater();
+                        break;
+                    case "3":
+                        myPet.Play();
+                        break;
+                    case "4":
+                        myPet.CleanWaste();
+                        break;
+                    case "5":
+                        myPet.TuckIn();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input");
+                        break;
+                }
+            }
+            while (true);
+
+        }
+
+        public static void QuitCheck(string quitInput)
+        {
+            if (quitInput.ToLower().Replace("\"", "") == "quit")
+            {
+                Console.WriteLine("Thank you for playing!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
         }
     }
 }
--- VirtualPet.cs
+++ VirtualPet.cs
@@ -20,6 +20,48 @@ namespace ShoskesVirtualPet
 
         //Properties
 
+        public string CatOrDog
+        {
+            get { return this.catOrDog; }
+            set { this.catOrDog = value; }
+        }
+
+        public string Name
+        {
+            get { return this.petName; }
+            set { this.petName = value; }
+        }
+
+        public int Nutrition
+        {
+            get { return this.nutrition;  }
+            set { this.nutrition = value; }
+        }
+
+        public int Hydration
+        {
+            get { return this.hydration; }
+            set { this.hydration = value; }
+        }
+
+        public int Socialization
+        {
+            get { return this.socialization; }
+            set { this.socialization = value; }
+        }
+
+        public int Cleanliness
+        {
+            get { return this.cleanliness; }
+            set { this.cleanliness = value; }
+        }
+
+        public int Energy
+        {
+            get { return this.energy; }
+            set { this.energy = value; }
+        }
+
         //Constructors
 
         public VirtualPet(string type, string name)
@@ -41,6 +83,100 @@ namespace ShoskesVirtualPet
 
         public void Feed()
         {
+            Console.WriteLine("You feed {0} a nutritious meal!", Name);
+            Nutrition += 3;
+            Hydration--;
+            Socialization++;
+            Cleanliness--;
+            Energy--;
+        }
+
+        public void ProvideWater()
+        {
+            Console.WriteLine("You offer {0} some cool, filtered water!", Name);
+            Nutrition++;
+            Hydration += 3;
+            Cleanliness--;
+         
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("You play with {0} and you both have fun!", Name);
+            Nutrition--;
+            Hydration--;
+            Socialization += 4;
+            Energy--;
+        }
+
+        public void CleanWaste()
+        {
+            Console.WriteLine("You clean up {0}'s waste. Stinky, but necessary...", Name);
+            Socialization--;
+            Cleanliness += 3;
+        }
+
+        public void TuckIn()
+        {
+            Console.WriteLine("You tuck {0} in for the night. Good night {0}!", Name);
+            Nutrition--;
+            Hydration--;
+            Socialization--;
+            Energy += 4;
+        }
+
+        public void StatusCheck()
+        {
+            if (Nutrition <= 0 || Hydration <= 0 || Socialization <= 0 || Cleanliness <= 0 || Energy <= 0)
+            {
+                Console.WriteLine("You haven't been taking care of {0} very well..", Name);
+                Console.WriteLine("Animal control was called in time, {0} is going to a better foster family.", Name);
+                Console.WriteLine("YOU LOSE");
+                Console.ReadKey();
+                Environment.Exit(0);
+
+            }
+            else if (Nutrition >= 20 && Hydration >= 20 && Socialization >= 20 && Cleanliness >= 20 && Energy >= 20)
+            {
+                Console.WriteLine("You took excellent care of {0}!", Name);
+                Console.WriteLine("A wonderful family is adopting them, and they'll always remember their time with you!");
+                Console.WriteLine("YOU WIN");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+        public void Tick()
+        {
+            StatusCheck();
+            Console.Write("\n\n\n\n\n");
+            if (catOrDog == "cat")
+            {
+                Console.WriteLine("  /\\___/\\");
+                Console.WriteLine(" ( ^   ^ )");
+                Console.WriteLine(" (  =-=   )");
+                Console.WriteLine(" (         )");
+                Console.WriteLine(" (          )");
+                Console.WriteLine(" ( oo   oo   )))))))))))");
+            }
+            else
+            {
+                Console.WriteLine("    ___  ");
+                Console.WriteLine("  //^ ^\\\\");
+                Console.WriteLine(" (/(_•_)\\)");
+                Console.WriteLine(" _/''*''\\_");
+                Console.WriteLine(" (,,,)^(,,,) ");
+            }
+            Console.Write("\n\n");
+            Console.WriteLine("Here is {0}'s current state:", Name);
+            Console.WriteLine();
+            Console.WriteLine("Nutrition is at: "+Nutrition);
+            Console.WriteLine("Hydration is at: " + Hydration);
+            Console.WriteLine("Socialization is at: " + Socialization);
+            Console.WriteLine("Cleanliness is at: " + Cleanliness);
+            Console.WriteLine("Energy is at: " + Energy);
+            Console.Write("\n\n");
+            Console.WriteLine("Press 1 to feed, 2 to provide water, 3 to play, 4 to clean up waste,");
+            Console.WriteLine("or 5 to tuck in for the night");
 
         }
     }
