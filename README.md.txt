25d5b9521a0c00e5dd5b14ef3575fcf790fe7849
valerieshoskes <indi.vs@gmail.com>
2016-09-16 15:34:42 -0400

    set up project

---
+++ README.md.txt
@@ -0,0 +1,26 @@
+The foster simulator virtual pet allows a "winnable" virtual pet game 
+where the pet gets to be adopted by a family. When you lose the game, the 
+pet is taken by animal control to go to a suitable foster. 
+
+First, a cat or dog is chosen and ASCII art is displayed. 
+
+The stats are:
+
+Nutrition:
+Hydration:
+Socialization:
+Cleanliness:
+Energy:
+
+They all start at 5. Getting them all to 20 wins the game, 
+dropping one to below 0 loses the game. 
+
+From the main menu, you have the options to:
+
+Provide Food
+Provide Water
+Play (low intensity)
+Play (high intensity)
+Clean up waste
+
+These actions will increase or decrease the stats. 
---
+++ VirtualPet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoskesVirtualPet
+{
+    class VirtualPet
+    {
+        //Fields
+        private string catOrDog;
+        private string petName;
+
+        private int nutrition;
+        private int hydration;
+        private int socialization;
+        private int cleanliness;
+        private int energy;
+
+        //Properties
+
+        //Constructors
+
+        public VirtualPet(string type, string name)
+        {
+            petName = name;
+            catOrDog = type;
+
+            nutrition = 5;
+            hydration = 5;
+            socialization = 5;
+            cleanliness = 5;
+            energy = 5;
+
+        }
+
+
+
+        //Methods
+
+        public void Feed()
+        {
+
+        }
+    }
+}
